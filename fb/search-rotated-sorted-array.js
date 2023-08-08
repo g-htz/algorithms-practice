5c3b6de4393cab195f7e056fe32a3ae1417c7cc7
George Hetrelezis <georgehetrelezis@gmail.com>
2023-08-08 11:22:45 +1000

    Code] Q33. Search Rotated Sorted Array solution

---
+++ fb/search-rotated-sorted-array.js
@@ -0,0 +1,22 @@
+/*
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/
+ * @lc app=leetcode id=33 lang=javascript
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let possible = 0;
+
+    for(i = 0; i < nums.length; i++) {
+        if(nums[i] == target) return i;
+    }
+
+    return -1;
+};
